951a8fa663f99f8c47812be0a447b621c964490b
Mark Joshwel <mark@joshwel.co>
2024-02-22 03:45:28 +0000

    restart!

--- src/nuko.zig
+++ src/nuko.zig
@@ -1,3 +1,34 @@
+// nuko: a small file uploader to [litter.]catbox.moe
+// --------------------------------------------------
+// mark joshwel <mark@joshwel.co>
+//
+//  This is free and unencumbered software released into the public domain.
+//
+//  Anyone is free to copy, modify, publish, use, compile, sell, or
+//  distribute this software, either in source code form or as a compiled
+//  binary, for any purpose, commercial or non-commercial, and by any
+//  means.
+//
+//  In jurisdictions that recognize copyright laws, the author or authors
+//  of this software dedicate any and all copyright interest in the
+//  software to the public domain. We make this dedication for the benefit
+//  of the public at large and to the detriment of our heirs and
+//  successors. We intend this dedication to be an overt act of
+//  relinquishment in perpetuity of all present and future rights to this
+//  software under copyright law.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+//  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+//
+//  For more information, please refer to <http://unlicense.org/>
+//
+// with all my heart, 2024 x
+
 const std = @import("std");
 const File = std.fs.File;
 const log = std.log;
@@ -7,370 +38,142 @@ const BUILD_HASH = "unknown";
 const BUILD_BRANCH = "unknown";
 const BUILD_DATE = "unknown";
 
-const OperationMode = enum {
-    catbox, // = "https://catbox.moe/user/api.php",
-    litterbox, // = "https://litterbox.catbox.moe/resources/internals/api.php",
+// unified tagged union struct used in nuko functions
+pub const Operation = union(OperationMode) {
+    litterbox: LitterboxOperation,
+    catbox_file: CatboxFileOperation,
+    catbox_album: CatboxAlbumOperation,
 };
 
-const Album = struct {
-    short: []const u8, // identifier part of album link
-    mode: AlbumMode,
-    files: std.ArrayList([]const u8),
+// operation mode enum to be used with the operation tagged union struct
+pub const OperationMode = enum {
+    litterbox,
+    catbox_file,
+    catbox_album,
 };
 
-const AlbumMode = enum {
+pub const LitterboxOperation = struct {
+    time: TimeValue = TimeValue.hour,
+};
+
+pub const CatboxFileOperation = struct {
+    userhash: []const u8 = "",
+    delete: bool = false,
+};
+
+pub const CatboxAlbumOperation = struct {
+    userhash: []const u8 = "",
+    album: Album,
+};
+
+pub const Album = struct {
+    short: []const u8 = "", // identifier part of album link
+    mode: AlbumMode = AlbumMode.createalbum,
+};
+
+pub const AlbumMode = enum {
     createalbum,
     editalbum,
     addtoalbum,
     removefromalbum,
+    deletealbum,
 };
 
-const TimeValue = enum(u8) {
+pub const TimeValue = enum(u8) {
     hour = 1,
     halfday = 12,
     day = 24,
     threedays = 72,
 };
 
-pub fn main() !void {
+pub const Behaviour = struct {
+    targets: std.ArrayList([]const u8),
+    operation: Operation,
+};
 
-    // argument handling
-    const allocator = std.heap.page_allocator;
+// argument handling function
+// - handles arguments (really? no way!)
+// - prints help and version information
+// - validates arguments (a la was a valid choice given to --time)
+// - if targets are local paths, checks if they exist
+fn handleArgs(_alloc: ?std.mem.Allocator) Behaviour {
+    var alloc: std.mem.Allocator = undefined;
 
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-
-    const help =
-        \\usage: {s} [-h] [-l] [-t {{1,12,24,72}}] [file, ...]
-        \\
-        \\a small file uploader to [litter.]catbox.moe
-        \\
-        \\positional arguments:
-        \\  file                path(s) to file to read
-        \\
-        \\optional arguments:
-        \\  -h, --help          show this help message and exit
-        \\  -v, --version       show program's version number and exit
-        \\  -l, --litterbox     use litterbox.catbox.moe instead of catbox.moe
-        \\  -t {{1,12,24,72}}, --time {{1,12,24,72}}
-        \\                      (for litterbox) how long the file should be kept
-        \\  -u USERHASH, --userhash USERHASH
-        \\                      (for catbox) specify a userhash for non-anonymous uploads
-        \\  -a [SHORT], --album [SHORT]
-        \\                      (for catbox) create a new album if short is not specified
-        \\                      add an '/edit' '/add' or 'remove' suffix to edit images,
-        \\                      add images to and remove images from an already existing
-        \\                      album respectively. 
-        \\                      when given, files parameter are shorts of files already on
-        \\                      catbox.
-        \\
-        \\                      shorts are the short identifiers of catbox files.
-        \\                      e.g., the short of 'https://files.catbox.moe/0k6bpc.jpg'
-        \\                      is '0k6bpc.jpg'
-        \\                      same goes for albums, but without the preceding '.../c/'
-        \\
-    ;
-
-    switch (args.len) {
-        0 => {
-            log.err(
-                \\recieved zero arguments not including program name
-                \\see '{s} --help' for more information.
-            , .{args[0]});
-            std.os.exit(255);
-        },
-        1 => {
-            log.err(
-                \\no arguments given
-                \\see '{s} --help' for more information.
-            , .{args[0]});
-            std.os.exit(1);
-        },
-        else => {},
-    }
-
-    var currently_a_time_arg: bool = false;
-    var currently_a_userhash_arg: bool = false;
-    var currently_a_album_arg: bool = false;
-
-    var option_mode: OperationMode = OperationMode.catbox;
-    var option_files = std.ArrayList([]const u8).init(allocator);
-    var option_litterbox_time: TimeValue = TimeValue.hour;
-    var option_catbox_userhash: []const u8 = "";
-    var option_catbox_is_album: bool = false;
-    var option_catbox_album: Album = Album{
-        .short = "",
-        .mode = AlbumMode.createalbum,
-        .files = option_files,
-    };
-    defer option_files.deinit();
-
-    for (args[1..]) |arg| {
-        if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help")) {
-            try std.io.getStdOut().writer().print(help, .{args[0]});
-            std.os.exit(0);
-        }
-
-        if (std.mem.eql(u8, arg, "-v") or std.mem.eql(u8, arg, "--version")) {
-            try std.io.getStdOut().writer().print(
-                "nuko v{s} ({s}@{s}, {s})\n",
-                .{ VERSION, BUILD_HASH, BUILD_BRANCH, BUILD_DATE },
-            );
-            std.os.exit(0);
-        }
-
-        if (std.mem.startsWith(u8, arg, "-t=") or std.mem.startsWith(u8, arg, "--time=") or currently_a_time_arg) {
-            if (std.mem.eql(u8, arg, "-l")) {
-                log.err(
-                    \\premature ending of time flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(3);
-            } else if (std.mem.eql(u8, arg, "--litterbox")) {
-                log.err(
-                    \\premature ending of time flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(3);
-            } else if (std.mem.eql(u8, arg, "-a")) {
-                log.err(
-                    \\premature ending of time flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(3);
-            } else if (std.mem.eql(u8, arg, "--album")) {
-                log.err(
-                    \\premature ending of time flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(3);
-            } else if (std.mem.eql(u8, arg, "-u")) {
-                log.err(
-                    \\premature ending of time flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(3);
-            } else if (std.mem.eql(u8, arg, "--userhash")) {
-                log.err(
-                    \\premature ending of time flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(3);
-            }
-
-            var split_iter = std.mem.splitBackwardsSequence(u8, arg, "=");
-
-            while (split_iter.next()) |sarg| {
-                if (std.mem.eql(u8, sarg, "1")) {
-                    option_litterbox_time = TimeValue.hour;
-                } else if (std.mem.eql(u8, sarg, "12")) {
-                    option_litterbox_time = TimeValue.halfday;
-                } else if (std.mem.eql(u8, sarg, "24")) {
-                    option_litterbox_time = TimeValue.day;
-                } else if (std.mem.eql(u8, sarg, "72")) {
-                    option_litterbox_time = TimeValue.threedays;
-                } else {
-                    log.err(
-                        \\invalid time value '{s}' (choose from 1, 12, 24, 72)
-                        \\see '{s} --help' for more information.
-                    , .{ sarg, args[0] });
-                    std.os.exit(3);
-                }
-                currently_a_time_arg = false;
-                break;
-            }
-            continue;
-        }
-
-        if (std.mem.startsWith(u8, arg, "-a=") or std.mem.startsWith(u8, arg, "--album=") or currently_a_album_arg) {
-            if (std.mem.eql(u8, arg, "-t")) {
-                currently_a_album_arg = false;
-                currently_a_time_arg = true;
-                continue;
-            } else if (std.mem.eql(u8, arg, "--time")) {
-                currently_a_album_arg = false;
-                currently_a_time_arg = true;
-                continue;
-            } else if (std.mem.eql(u8, arg, "-u")) {
-                currently_a_album_arg = false;
-                currently_a_userhash_arg = true;
-                continue;
-            } else if (std.mem.eql(u8, arg, "--userhash")) {
-                currently_a_album_arg = false;
-                currently_a_userhash_arg = true;
-                continue;
-            }
-
-            var split_iter = std.mem.splitBackwardsSequence(u8, arg, "=");
-            var i: isize = 0;
-
-            while (split_iter.next()) |sarg| {
-                var split_album_iter = std.mem.splitBackwardsSequence(u8, sarg, "/");
-                while (split_album_iter.next()) |sam| {
-                    // xxxxx/{edit,add,remove}
-                    if (i == 0) {
-                        option_catbox_album.short = sam;
-                    } else if (i == 2) {
-                        break;
-                    }
-
-                    if (std.mem.eql(u8, sam, "edit")) {
-                        option_catbox_album.mode = AlbumMode.editalbum;
-                    } else if (std.mem.eql(u8, sam, "add")) {
-                        option_catbox_album.mode = AlbumMode.addtoalbum;
-                    } else if (std.mem.eql(u8, sam, "remove")) {
-                        option_catbox_album.mode = AlbumMode.removefromalbum;
-                    } else {
-                        option_catbox_album.short = sam;
-                    }
-                }
-                break;
-            }
-            currently_a_album_arg = false;
-            continue;
-        }
-
-        if (std.mem.startsWith(u8, arg, "-u=") or std.mem.startsWith(u8, arg, "--userhash=") or currently_a_userhash_arg) {
-            if (std.mem.eql(u8, arg, "-l")) {
-                log.err(
-                    \\premature ending of userhash flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(4);
-            } else if (std.mem.eql(u8, arg, "--litterbox")) {
-                log.err(
-                    \\premature ending of userhash flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(4);
-            } else if (std.mem.eql(u8, arg, "-a")) {
-                log.err(
-                    \\premature ending of userhash flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(4);
-            } else if (std.mem.eql(u8, arg, "--album")) {
-                log.err(
-                    \\premature ending of userhash flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(4);
-            } else if (std.mem.eql(u8, arg, "-t")) {
-                log.err(
-                    \\premature ending of userhash flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(4);
-            } else if (std.mem.eql(u8, arg, "--time")) {
-                log.err(
-                    \\premature ending of userhash flag
-                    \\see '{s} --help' for more information.
-                , .{args[0]});
-                std.os.exit(4);
-            }
-
-            var split_iter = std.mem.splitBackwardsSequence(u8, arg, "=");
-
-            while (split_iter.next()) |sarg| {
-                option_catbox_userhash = sarg;
-                currently_a_userhash_arg = false;
-                break;
-            }
-            continue;
-        }
-
-        if (std.mem.eql(u8, arg, "-t") or std.mem.eql(u8, arg, "--time")) {
-            currently_a_time_arg = true;
-            continue;
-        } else {
-            currently_a_time_arg = false;
-        }
-
-        if (std.mem.eql(u8, arg, "-u") or std.mem.eql(u8, arg, "--u")) {
-            currently_a_userhash_arg = true;
-            continue;
-        } else {
-            currently_a_userhash_arg = false;
-        }
-
-        if (std.mem.eql(u8, arg, "-a") or std.mem.eql(u8, arg, "--album")) {
-            currently_a_album_arg = true;
-            option_catbox_is_album = true;
-            continue;
-        } else {
-            currently_a_album_arg = false;
-        }
-
-        if (std.mem.eql(u8, arg, "-l") or std.mem.eql(u8, arg, "--litterbox")) {
-            option_mode = OperationMode.litterbox;
-            continue;
-        }
-
-        try option_files.append(arg[0..arg.len]);
-    }
-
-    if (currently_a_time_arg) {
-        log.err(
-            \\premature ending of time flag
-            \\see '{s} --help' for more information.
-        , .{args[0]});
-        std.os.exit(3);
-    } else if (currently_a_userhash_arg) {
-        log.err(
-            \\premature ending of userhash flag
-            \\see '{s} --help' for more information.
-        , .{args[0]});
-        std.os.exit(4);
-    }
-
-    // log.debug("operation_mode={any}", .{option_mode});
-    // log.debug("litterbox_time={any}", .{option_litterbox_time});
-    // log.debug("catbox_userhash={any}", .{option_catbox_userhash});
-    // log.debug("catbox_is_album={any}", .{option_catbox_is_album});
-    // log.debug("catbox_album={any}", .{option_catbox_album});
-    // log.debug("files={any}", .{option_files});
-
-    // check if any files were given
-    if (option_files.items.len == 0) {
-        log.err(
-            \\no files given
-            \\see '{s} --help' for more information.
-        , .{args[0]});
-        std.os.exit(5);
-    }
-
-    // TODO: check if files exists
-    for (option_files.items) |target| {
-        _ = target;
-    }
-
-    if (option_catbox_is_album) {
-        manageAlbum(option_catbox_album, option_catbox_userhash);
+    if (_alloc == null) {
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        alloc = gpa.allocator();
+        defer gpa.deinit();
     } else {
-        for (option_files.items) |file| {
-            uploadFile(file, option_mode, option_litterbox_time, option_catbox_userhash);
-        }
+        alloc = _alloc.?;
     }
-    // upload files
+
+    return Behaviour{
+        .targets = std.ArrayList([]const u8).init(alloc),
+        .operation = Operation{
+            .litterbox = LitterboxOperation{
+                .time = TimeValue.hour,
+            },
+        },
+    };
 }
 
-fn uploadFile(path: []const u8, mode: OperationMode, time: TimeValue, userhash: []const u8) void {
-    _ = userhash;
-    _ = mode;
-    _ = time;
+// uploads data to litterbox/catbox
+// arguments:
+//   data: []u8
+//     data to upload
+//   operation: Operation
+//     operation to perform on the data
+pub fn uploadData(data: []u8, operation: Operation, _alloc: ?std.mem.Allocator) bool {
+    var alloc: std.mem.Allocator = undefined;
 
-    nosuspend std.io.getStdErr().writer().print("uploading '{s}'... ", .{path}) catch return;
+    if (_alloc == null) {
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        alloc = gpa.allocator();
+        defer gpa.deinit();
+    } else {
+        alloc = _alloc.?;
+    }
 
-    // TODO
-
-    nosuspend std.io.getStdErr().writer().print("done\n", .{}) catch return;
-}
-
-fn manageAlbum(album: Album, userhash: []const u8) void {
-    _ = album;
-    _ = userhash;
+    _ = data;
+    _ = operation;
 
     // TODO
     log.err("album management is not implemented yet", .{});
     std.os.exit(255);
+
+    return true;
+}
+
+// manages a catbox album
+// arguments:
+//   file_shorts: std.ArrayList([]const u8)
+//      list of file short identifiers
+//  operation: Operation
+//      operation to perform on the album
+pub fn manageAlbum(file_shorts: std.ArrayList([]const u8), operation: Operation, _alloc: ?std.mem.Allocator) void {
+    var alloc: std.mem.Allocator = undefined;
+
+    if (_alloc == null) {
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        alloc = gpa.allocator();
+        defer gpa.deinit();
+    } else {
+        alloc = _alloc.?;
+    }
+
+    _ = file_shorts;
+    _ = operation;
+
+    // TODO
+    log.err("album management is not implemented yet", .{});
+    std.os.exit(255);
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    defer gpa.deinit();
+
+    const operation = handleArgs(alloc);
+    _ = operation;
 }
